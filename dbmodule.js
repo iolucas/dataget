b12f004bce2e273be7f994ac13066c64d89bdf87
Lucas Oliveira <lucas.o@live.com>
2024-01-28 00:51:02 -0300

    Updated mongodb driver version; Updated driver functions calling to use promises; Updated dependencies files

--- dbmodule.js
+++ dbmodule.js
@@ -2,84 +2,52 @@ var MongoClient = require('mongodb').MongoClient;
 var moment = require("moment-timezone");
 
 // Connection url
-var url = 'mongodb://localhost:27017/test';
-//var url = "mongodb://46460205-f620-424d-b19f-3e47e5374fa5:81b56f61-adbe-434b-b5e5-cb2a01917423@192.155.243.53:10026/db";
-//Create system to drop to a sqlite db in case no mongodb is accessible
-
+var url = 'mongodb://localhost:27017';
 
 var collectionRef = null;
 
 module.exports = {
 
     init: function(callback) {
+
         // Connect using MongoClient
-        MongoClient.connect(url, function(err, db) {
+        MongoClient.connect(url).then((client) => {
             // Create a collection we want to drop later
+            db = client.db('test');
             collectionRef = db.collection('pagefragUrls');
+
+            callback(null);
+        }).catch((err) => {
+            console.log(err);
             callback(err);
         });
     },
 
-    insert: function(data, callback) {
+    insert: (data, callback) => {
         data.createdOn = moment().tz("America/Sao_Paulo").format('llll');
 
-        collectionRef.insertOne(data, null, function(err, result) {
+        collectionRef.insertOne(data).then((result) => {
+            // console.log(result);
+            callback(null);
+        }).catch((err) => {
             callback(err);
         });
     },
 
     get: function(urlPath, callback) {
-        collectionRef.findOne({path: urlPath},function(err, result) {
-            callback(err, result);
+        collectionRef.findOne({path: urlPath}).then(function(result) {
+            callback(null, result);
+        }).catch((err) => {
+            callback(err, null);
         });
     },
 
     listAll: function(callback) {
-        collectionRef.find({}).toArray(function(err, results) {
-            callback(err, results);
+        collectionRef.find({}).toArray().then(function(results) {
+            callback(null, results);
+        }).catch((err) => {
+            callback(err, null);
         });
+
     }
 }
-
-
-// Connect using MongoClient
-/*MongoClient.connect(url, function(err, db) {
-    // Create a collection we want to drop later
-    var col = db.collection('createIndexExample1');
-    col.findOne({a:1},function(err, items) {
-        console.log(items)
-        //test.equal(null, err);
-        //test.equal(4, items.length);
-        db.close();
-    });
-
-});*/
-/*
-{
-  "mongodb": [
-    {
-      "credentials": {
-        "hostname": "192.155.243.53",
-        "host": "192.155.243.53",
-        "port": 10026,
-        "username": "46460205-f620-424d-b19f-3e47e5374fa5",
-        "password": "81b56f61-adbe-434b-b5e5-cb2a01917423",
-        "name": "328ead99-b4fb-4198-a557-ae2b92342953",
-        "db": "db",
-        "url": "mongodb://46460205-f620-424d-b19f-3e47e5374fa5:81b56f61-adbe-434b-b5e5-cb2a01917423@192.155.243.53:10026/db"
-      },
-      "syslog_drain_url": null,
-      "label": "mongodb",
-      "provider": "core",
-      "plan": "100",
-      "name": "mongodb-6r",
-      "tags": [
-        "nosql",
-        "document",
-        "mongodb",
-        "data_management",
-        "ibm_experimental"
-      ]
-    }
-  ]
-}*/
